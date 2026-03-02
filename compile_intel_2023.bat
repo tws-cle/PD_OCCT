b5b7c46e572e3fa95f0030bc70cd9376af25c689
Chau Le <Chau.Le@thirdwavesys.com>
2026-03-02 16:29:36 -0600

    install at designated folder

--- compile_intel_2023.bat
+++ compile_intel_2023.bat
@@ -2,4 +2,4 @@ call "C:\Program Files (x86)\Intel\oneAPI\setvars.bat"
 call "git config --global --add safe.directory *"
 cmake -C C:\pd_occt\intel_2023.cmake  -S . -B C:\build  -G "Visual Studio 17 2022" -A x64 -T "Intel C++ Compiler 2023" -DCMAKE_CXX_STANDARD=14
 cmake --build C:\build --config Release --verbose
-cmake --install C:\install\release_intel_2023
+cmake --install C:\build --config Release --prefix "C:/occt_install/OCCT_intel_2023"
